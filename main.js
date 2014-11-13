1ed1076045f97b552b017fc2845d5e5202f56e6b
Алексей Новиков <anovikov@wezzet.com>
2014-11-14 00:15:57 +0300

    add body class extension name

--- main.js
+++ main.js
@@ -6,8 +6,19 @@ define(function(require, exports, module) {
     AppInit.appReady(function () {
         ExtensionUtils.loadStyleSheet(module, 'styles/style.css');
         
-        var $projectFilesHeader     = document.getElementById('project-files-header'),
+        var $body                   = document.body,
+            $projectFilesHeader     = document.getElementById('project-files-header'),
             $projectFilesContainer  = document.getElementById('project-files-container');
+        
+        $body.classList.add('bft-extension');
+        
+        if(!window.lamo2k123) {
+            window.lamo2k123 = [];
+        }
+        
+        // @TODO: config
+        window.lamo2k123.push('lamo2k123.brackets-file-tree');
+        
         
         var $button = document.createElement('a');
         $button.classList.add('bft-toggle-type');
